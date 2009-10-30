a1723f8ae97059928bea54d87045ab12c30108bf
Simon Käser <skaeser@raptus.com>
2009-10-30 13:23:00 +0000

    updated README and egg description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.1'
 
 setup(name='raptus.browsermessage',
       version=version,
-      description="",
+      description="Configurable upgrade message for visitors using a outdated browser",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
